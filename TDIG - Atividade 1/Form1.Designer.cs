6f824ab08db3a1bd9363ca13e1e20f088fe95ba1
Lavyk Soares Nunes <lavyk_@hotmail.com>
2019-10-05 01:58:47 -0300

    Adicionar arquivos de projeto.

---
+++ TDIG - Atividade 1/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace TDIG___Atividade_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.botaoAtualizar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(18)))), ((int)(((byte)(64)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.MaxLength = 50;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 50);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(18)))), ((int)(((byte)(64)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox2.Location = new System.Drawing.Point(13, 69);
+            this.textBox2.MaxLength = 50;
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(191, 50);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(18)))), ((int)(((byte)(64)))));
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Location = new System.Drawing.Point(13, 125);
+            this.textBox3.MaxLength = 50;
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(191, 50);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(18)))), ((int)(((byte)(64)))));
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox4.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox4.Location = new System.Drawing.Point(12, 181);
+            this.textBox4.MaxLength = 50;
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(192, 50);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(18)))), ((int)(((byte)(64)))));
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox5.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox5.Location = new System.Drawing.Point(13, 237);
+            this.textBox5.MaxLength = 50;
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(191, 50);
+            this.textBox5.TabIndex = 6;
+            // 
+            // botaoAtualizar
+            // 
+            this.botaoAtualizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(145)))), ((int)(((byte)(71)))), ((int)(((byte)(255)))));
+            this.botaoAtualizar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.botaoAtualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoAtualizar.ForeColor = System.Drawing.Color.White;
+            this.botaoAtualizar.Location = new System.Drawing.Point(13, 293);
+            this.botaoAtualizar.Name = "botaoAtualizar";
+            this.botaoAtualizar.Size = new System.Drawing.Size(359, 56);
+            this.botaoAtualizar.TabIndex = 1;
+            this.botaoAtualizar.Text = "Atualizar";
+            this.botaoAtualizar.UseVisualStyleBackColor = false;
+            this.botaoAtualizar.Click += new System.EventHandler(this.BotaoAtualizar_Click);
+            // 
+            // button1
+            // 
+            this.button1.AccessibleName = "Botão 1";
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(166)))), ((int)(((byte)(255)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(210, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Botão 1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.AccessibleName = "Botão 2";
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(166)))), ((int)(((byte)(255)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(211, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 50);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Botão 2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.AccessibleName = "Botão 3";
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(166)))), ((int)(((byte)(255)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(210, 125);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 50);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Botão 3";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.AccessibleName = "Botão 4";
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(166)))), ((int)(((byte)(255)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(211, 181);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(161, 50);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Botão 4";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.AccessibleName = "Botão 5";
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(166)))), ((int)(((byte)(255)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(210, 237);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(161, 50);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Botão 5";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(36)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.botaoAtualizar);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TDIG - Atividade 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button botaoAtualizar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ TDIG - Atividade 1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TDIG___Atividade_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Função Hint no C#
+            ToolTip ToolTip1 = new ToolTip();
+
+            ToolTip1.SetToolTip(this.textBox1, "TextBox 1");
+            ToolTip1.SetToolTip(this.textBox2, "TextBox 2");
+            ToolTip1.SetToolTip(this.textBox3, "TextBox 3");
+            ToolTip1.SetToolTip(this.textBox4, "TextBox 4");
+            ToolTip1.SetToolTip(this.textBox5, "TextBox 5");
+
+            ToolTip1.SetToolTip(this.button1, "Botão 1");
+            ToolTip1.SetToolTip(this.button2, "Botão 2");
+            ToolTip1.SetToolTip(this.button3, "Botão 3");
+            ToolTip1.SetToolTip(this.button4, "Botão 4");
+            ToolTip1.SetToolTip(this.button5, "Botão 5");
+
+            ToolTip1.SetToolTip(this.botaoAtualizar, "Atualizar botões");
+        }
+
+        
+        private void BotaoAtualizar_Click(object sender, EventArgs e) // Quando clickar no botãoAtualizar
+        {
+            // Chama a função para atualizar cada botão.
+            this.Atualizar(this.textBox1, this.button1);
+            this.Atualizar(this.textBox2, this.button2);
+            this.Atualizar(this.textBox3, this.button3);
+            this.Atualizar(this.textBox4, this.button4);
+            this.Atualizar(this.textBox5, this.button5);
+        }
+
+        private void Atualizar(TextBox txt, Button btn) //Parametros: respectivos textBox e botão.
+        {
+            //Verifica se o textBox ta vazio
+            String txtTemp; // Variavel temporaria
+            if (txt.Text == "") // Se tiver vazio pega o nome normal do botão e salva na variavel temporaria.
+            {
+                txtTemp = btn.AccessibleName;
+            }
+            else // Se não estiver vazio, pega o texto do textBox e salva na variavel temporaria.
+            {
+                txtTemp = txt.Text;
+            }
+            // Atualiza o texto do botão
+            btn.Text = txtTemp;
+        }
+    }
+}
